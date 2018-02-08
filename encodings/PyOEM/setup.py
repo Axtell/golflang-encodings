d9bb7729a5252f1b8f8d6ec063e25f5a5b8205e0
Mego <mega.gamer05@gmail.com>
2018-02-08 07:31:38 -0600

    Reformat structure

---
+++ encodings/PyOEM/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""PyOEM - A Python codec for the IBM CP437 code page (OEM-US)"""
+
+from setuptools import setup
+
+setup(
+    name='pyoem',
+
+    version='1.0',
+
+    description='A Python codec for the IBM CP437 code page (OEM-US)',
+
+    url='https://github.com/Mego/PyOEM',
+
+    author='Mego',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='text encoding unicode',
+)
